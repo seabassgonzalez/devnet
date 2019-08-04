b6ae84fda6523822f102a2ed3fb25c176c4fbb13
seabassgonzalez <seabassgonzalez@gmail.com>
2019-08-04 14:11:34 -0700

    Format model and export

--- models/Profile.js
+++ models/Profile.js
@@ -28,84 +28,86 @@ const ProfileSchema = new mongoose.Schema({
 	githubusername: {
 		type:String
 	},
-	  experience: [
+	experience: [
 	    {
-	      title: {
-	        type: String,
-	        required: true
-	      },
-	      company: {
-	        type: String,
-	        required: true
-	      },
-	      location: {
-	        type: String
-	      },
-	      from: {
-	        type: Date,
-	        required: true
-	      },
-	      to: {
-	        type: Date
-	      },
-	      current: {
-	        type: Boolean,
-	        default: false
-	      },
-	      description: {
-	        type: String
-	      }
+	    	title: {
+	       	 	type: String,
+	        	required: true
+	    	},
+	    	company: {
+	    		type: String,
+	        	required: true
+	    	},
+	    	location: {
+	       		type: String
+	    	},
+	    	from: {
+	        	type: Date,
+	        	required: true
+	    	},
+	    	to: {
+	        	type: Date
+	    	},
+	    	current: {
+	       		type: Boolean,
+	        	default: false
+	    	},
+	    	description: {
+	        	type: String
+	    	}
 	    }
-	  ],
-	  education: [
+	],
+	education: [
 	    {
-	      school: {
-	        type: String,
-	        required: true
-	      },
-	      degree: {
-	        type: String,
-	        required: true
-	      },
-	      fieldofstudy: {
-	        type: String,
-	        required: true
-	      },
-	      from: {
-	        type: Date,
-	        required: true
-	      },
-	      to: {
-	        type: Date
-	      },
-	      current: {
-	        type: Boolean,
-	        default: false
-	      },
-	      description: {
-	        type: String
-	      }
+	    	school: {
+	        	type: String,
+	        	required: true
+	      	},
+	      	degree: {
+	        	type: String,
+	        	required: true
+	      	},
+	      	fieldofstudy: {
+	        	type: String,
+	        	required: true
+	      	},
+	      	from: {
+	        	type: Date,
+	        	required: true
+	      	},
+	      	to: {
+	        	type: Date
+	      	},
+	      	current: {
+	        	type: Boolean,
+	        	default: false
+	      	},
+	      	description: {
+	        	type: String
+	      	}
 	    }
-	  ],
-  social: {
-    youtube: {
-      type: String
-    },
-    twitter: {
-      type: String
-    },
-    facebook: {
-      type: String
-    },
-    linkedin: {
-      type: String
-    },
-    instagram: {
-      type: String
-    }
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+	],
+  	social: {
+    	youtube: {
+      		type: String
+    	},
+    	twitter: {
+      		type: String
+    	},
+    	facebook: {
+      		type: String
+    	},
+    	linkedin: {
+      		type: String
+    	},
+    	instagram: {
+      		type: String
+    	}
+  	},
+  	date: {
+    	type: Date,
+    	default: Date.now
+  	}
 });
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
