7319a1799e172b5ab4183fbae1c76afc537fdc6e
seabassgonzalez <seabassgonzalez@gmail.com>
2019-08-06 15:46:44 -0700

    Initial create profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -20,6 +21,20 @@ router.get('/me', auth, async (req, res) => {
 	} catch(err) {
 		console.error(err.message);
 		res.status(500).send('Server Error');
+	}
+});
+
+// @route 	POST api/profile
+// @desc 	Create or update user profile
+// @access 	Private
+router.post('/', [auth, [
+	check('status', 'Status is required').not().isEmpty(),
+	check('skills', 'Skills is required').not().isEmpty()
+	] 
+], async (req, res) => {
+	const errors = validationResult(req);
+	if(!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
 	}
 });
 
