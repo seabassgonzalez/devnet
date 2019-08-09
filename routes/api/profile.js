18bc126e30097572c9a79ca68b5de3d457d7b7b4
seabassgonzalez <seabassgonzalez@gmail.com>
2019-08-09 12:18:39 -0700

    Fix deprecated method, create get profiles route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -92,9 +92,21 @@ router.post('/', [auth, [
 		res.json(profile);
 	} catch(err){
 		console.error(err.message);
-		
+
 		res.status(500).send('Server Error');
 	}
 });
 
+// @route 	GET api/profile
+// @desc 	Get all profiles
+// @access 	Public
+router.get('/', async (req, res) => {
+	try {
+		const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+		res.json(profiles);
+	} catch (err) {
+		console.error(err.message);
+		rest.status(500).send('Server Error');
+	}
+});
 module.exports = router;
