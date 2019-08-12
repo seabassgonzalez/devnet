ad32cb2aba6bbfbfda9bd95c107d47f6535de1c5
seabassgonzalez <seabassgonzalez@gmail.com>
2019-08-11 22:26:52 -0700

    Find profile by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -115,14 +115,17 @@ router.get('/', async (req, res) => {
 // @access 	Public
 router.get('/user/:user_id', async (req, res) => {
 	try {
-		const profiles = await Profile.find({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+		const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
 			
-		if(!profile) return res.status(400).json({ msg: 'There is no profile for this user' });
+		if(!profile) return res.status(400).json({ msg: 'Profile not found' });
 
 		res.json(profile);
 	} catch (err) {
 		console.error(err.message);
-		rest.status(500).send('Server Error');
+		if(err.kind === 'ObjectId'){
+			return res.status(400).json({ msg: 'Profile not found' });
+		}
+		res.status(500).send('Server Error');
 	}
 });
 
