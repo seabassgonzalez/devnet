0130f68bb9bb52acf1190b3ff62706455f7ae630
seabassgonzalez <seabassgonzalez@gmail.com>
2019-08-20 18:48:07 -0700

    Get all posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -29,12 +29,25 @@ router.post('/', [ auth, [check('text', 'Text is required').not().isEmpty()]], a
 		const post = await newPost.save();
 
 		res.json(post);
-	} catch (err){
+	} catch (err) {
 		console.error(err.message);
 		res.status(500).send('Server Error');
 	}
 
 	
+});
+
+// @route 	GET api/posts
+// @desc 	Get all posts
+// @access 	Private
+router.get('/', auth, async (req, res) => {
+	try {
+		const posts = await Post.find().sort({ date: -1 });
+		res.json(posts);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 module.exports = router;
