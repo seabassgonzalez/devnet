9c0fa1fb58dc98c606a663a8a8c44fad64ea11aa
seabassgonzalez <seabassgonzalez@gmail.com>
2019-08-23 15:55:50 -0700

    Delete comment from post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -175,8 +175,41 @@ router.post('/comment/:id', [ auth, [check('text', 'Text is required').not().isE
 		console.error(err.message);
 		res.status(500).send('Server Error');
 	}
-
 	
+});
+
+// @route 	DELETE api/posts/comment/:id/:comment_id
+// @desc 	Delete comment
+// @access 	Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// pull out comment from post
+		const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+		// make sure comment exists
+		if(!comment){
+			return res.status(404).json({ msg: 'Comment does not exist' });
+		}
+
+		// check if user deleting comment is user that created it
+		if(comment.user.toString() !== req.user.id){
+			return res.status(401).json({ msg: 'User not authorized' });
+		}
+
+		// get remove index
+		const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+
+		post.comments.splice(removeIndex, 1);
+
+		await post.save();
+
+		res.json(post.comments);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 module.exports = router;
