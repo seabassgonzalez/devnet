4432f6df7fbe03e6c370d53ff8a1ba25deffc7f1
seabassgonzalez <seabassgonzalez@gmail.com>
2019-08-25 15:35:33 -0700

    mport Router Route and Switch wrap components to be rendered in Router, initial Login and Register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={ Landing } />
+    </Fragment>
+  </Router>
 );
 
 export default App;
