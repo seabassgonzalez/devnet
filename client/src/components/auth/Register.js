cc191693559c7eaa24bd966608f531d5491fcd15
seabassgonzalez <seabassgonzalez@gmail.com>
2019-08-28 16:01:05 -0700

    Corrected onChange invocation, apostrophes used throughout

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -10,7 +10,7 @@ const Register = () => {
 
 	const { name, email, password, password2 } = formData;
 
-	const onChange = e => setFormData({ ...formData, name: e.target.value });
+	const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
 	const onSubmit = e => {
 		e.preventDefault();
@@ -23,28 +23,28 @@ const Register = () => {
 
 	return (
 		<Fragment>
-			<h1 className="large text-primary">
+			<h1 className='large text-primary'>
 				Sign Up
 			</h1>
-	  		<p className="lead">
-	  			<i className="fas fa-user"></i> Create Your Account
+	  		<p className='lead'>
+	  			<i className='fas fa-user'></i> Create Your Account
 	  		</p>
-	      	<form className="form" onSubmit={e => onSubmit(e)}>
-		        <div className="form-group">
+	      	<form className='form' onSubmit={e => onSubmit(e)}>
+		        <div className='form-group'>
 		          	<input 
-		          		type="text" 
-		          		placeholder="Name" 
-		          		name="name" 
+		          		type='text' 
+		          		placeholder='Name' 
+		          		name='name' 
 		          		value={name} 
 		          		onChange={e => onChange(e)} 
 		          		required 
 		          	/>
 		        </div>
-		        <div className="form-group">
+		        <div className='form-group'>
 		          	<input 
-		          		type="email" 
-		          		placeholder="Email Address" 
-		          		name="email" 
+		          		type='email' 
+		          		placeholder='Email Address' 
+		          		name='email' 
 		          		value={email} 
 		          		onChange={e => onChange(e)} 
 		          		required 
@@ -53,30 +53,30 @@ const Register = () => {
 		          		This site uses Gravatar so if you want a profile image, use a Gravatar email
 		            </small>
 		        </div>
-		        <div className="form-group">
+		        <div className='form-group'>
 		          	<input
-			            type="password"
-			            placeholder="Password"
-			            name="password"
+			            type='password'
+			            placeholder='Password'
+			            name='password'
 			            value={password} 
 			            onChange={e => onChange(e)}
-			            minLength="6"
+			            minLength='6'
 		          	/>
 		        </div>
-		        <div className="form-group">
+		        <div className='form-group'>
 		        	<input
-			            type="password"
-			            placeholder="Confirm Password"
-			            name="password2"
+			            type='password'
+			            placeholder='Confirm Password'
+			            name='password2'
 			            value={password2} 
 			            onChange={e => onChange(e)}
-			            minLength="6"
+			            minLength='6'
 		          	/>
 		        </div>
-	        	<input type="submit" className="btn btn-primary" value="Register" />
+	        	<input type='submit' className='btn btn-primary' value='Register' />
 	      	</form>
-	      	<p className="my-1">
-	        	Already have an account? <a href="login.html">Sign In</a>
+	      	<p className='my-1'>
+	        	Already have an account? <a href='login.html'>Sign In</a>
 	      	</p>
 		</Fragment>
 )};
