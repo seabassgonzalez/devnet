65b2786122c921469929bc4987b220e73f78160f
seabassgonzalez <seabassgonzalez@gmail.com>
2019-08-31 01:18:47 -0700

    Implement initial redux store

---
+++ client/src/store.js
@@ -0,0 +1,16 @@
+import { createStore, appyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+	rootReducer,
+	initialState,
+	composeWithDevTools(appyMiddleware(...middleware))
+);
+
+export default store;
