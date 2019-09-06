f42ddee7bc3e047872bce13e9fe4b25df784b929
seabassgonzalez <seabassgonzalez@gmail.com>
2019-09-06 11:46:41 -0700

    Trying to fix proxy bug, working backwards

--- client/src/App.js
+++ client/src/App.js
@@ -6,31 +6,19 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import './App.css';
 
-// redux
-import { Provider } from 'react-redux';
-import store from './store';
-import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken'; 
-
 const App = () => (
-  useEffect(()=> {
-    store.dispatch(loadUser());
-  }, []);
-
-  return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={ Landing } />
-        <section className='container'>
-          <Switch>
-            <Route exact path='/register' component={ Register } />
-            <Route exact path='/login' component={ Login } />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-    );
-};
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={ Landing } />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' component={ Register } />
+          <Route exact path='/login' component={ Login } />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+);
 
 export default App;
