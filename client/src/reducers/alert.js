e85feae3baf55f35621e2c295db2fe31353f16b5
seabassgonzalez <seabassgonzalez@gmail.com>
2019-09-06 15:28:02 -0700

    Destructure action out of action.type and action.payload

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -5,11 +5,13 @@ const initialState = [
 ];
 
 export default function(state = initialState, action){
-	switch(action.type){
+	const { type, payload } = action;
+
+	switch(type){
 		case 'SET_ALERT':
-			return [...state, action.payload];
+			return [...state, payload];
 		case 'REMOVE_ALERT': 
-			return state.filter(alert => alert.id !== action.payload);
+			return state.filter(alert => alert.id !== payload);
 		default: 
 			return state; 
 	}
