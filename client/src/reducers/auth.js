63e8e2630134d6406658987e13587e6a17158fd6
seabassgonzalez <seabassgonzalez@gmail.com>
2019-09-09 21:55:38 -0700

    Implement auth action, import auth action, implement in reducer

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,12 +1,6 @@
 import {
 	REGISTER_SUCCESS,
-	REGISTER_FAIL,
-	USER_LOADED,
-	AUTH_ERROR,
-	LOGIN_SUCCESS,
-	LOGIN_FAIL,
-	LOGOUT,
-	ACCOUNT_DELETED
+	REGISTER_FAIL
 } from '../actions/types';
 
 const initialState = {
@@ -20,34 +14,22 @@ export default function(state = initialState, action) {
 	const { type, payload } = action;
 
 	switch(type) {
-		case USER_LOADED:
-			return {
-				...state,
-				isAuthenticated: true,
-				loading: false,
-				user: payload
-			};
 		case REGISTER_SUCCESS:
-		case LOGIN_SUCCESS: 
 			localStorage.setItem('token', payload.token);
 			return {
 				...state,
 				...payload,
 				isAuthenticated: true,
 				loading: false
-			};
-		case REGISTER_FAIL:
-		case AUTH_ERROR:
-		case LOGIN_FAIL:
-		case LOGOUT: 
-		case ACCOUNT_DELETED:
+			}
+		case REGISTER_FAIL: 
 			localStorage.removeItem('token');
 			return {
 				...state,
-				token: null,
+				token: null, 
 				isAuthenticated: false,
 				loading: false
-			};
+			}
 			default: 
 				return state;
 	}
