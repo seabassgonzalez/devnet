96fd385d45e2b3bce03343f2461149b15aeb7c7b
seabassgonzalez <seabassgonzalez@gmail.com>
2019-09-19 22:14:13 -0700

    Display different profile content if no profile data yet entered

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
@@ -14,7 +15,16 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
 		<p className="lead">
 			<i className="fas fa-user"></i> Welcome { user && user.name }
 		</p>
-		{profile !== null ? <Fragment>has</Fragment> : <Fragment>has not</Fragment>}
+		{profile !== null ? (
+			<Fragment>has</Fragment>
+		) : (
+			<Fragment>
+				<p>You have not yet setup a profile, please add some info</p>
+				<Link to='/create-profile' className='btn btn-primary my-1'>
+					Create Profile
+				</Link>
+			</Fragment>
+		)}
 	</Fragment>;
 };
 
