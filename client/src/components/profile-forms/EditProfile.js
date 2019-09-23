dd7c11b828c80dd2616d195c704ea48229d18f51
seabassgonzalez <seabassgonzalez@gmail.com>
2019-09-22 20:30:47 -0700

    Change back button to link

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -150,7 +150,7 @@ const EditProfile = ({ profile: { profile, loading }, createProfile, getCurrentP
 		        </Fragment>}
 
 	        	<input type="submit" className="btn btn-primary my-1" />
-	        	<a className="btn btn-light my-1" href="dashboard.html">Go Back</a>
+	        	<Link className="btn btn-light my-1" to="/dashboard">Go Back</Link>
 	      	</form>
  		</Fragment>
 	)
