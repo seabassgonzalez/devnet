fd59d931d31abdcdfaa46e64dbd8201f7ce31e39
seabassgonzalez <seabassgonzalez@gmail.com>
2019-09-23 17:48:26 -0700

    Implement AddEducation action, create route for it in app

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 // redux
@@ -42,7 +43,7 @@ const App = () => {
             <PrivateRoute exact path='/dashboard' component={ Dashboard } />
             <PrivateRoute exact path='/create-profile' component={ CreateProfile } />
             <PrivateRoute exact path='/edit-profile' component={ EditProfile } />
-            <PrivateRoute exact path='/add-experience' component={ AddExperience } />
+            <PrivateRoute exact path='/add-education' component={ AddEducation } />
           </Switch>
         </section>
       </Fragment>
