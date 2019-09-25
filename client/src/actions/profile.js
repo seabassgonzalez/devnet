a62f646aefad7f445136b1ee490ab4e4a11dcd05
seabassgonzalez <seabassgonzalez@gmail.com>
2019-09-25 11:21:32 -0700

    Implement delete education action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -143,3 +143,22 @@ export const deleteExperience = id => async dispatch => {
 		});
 	}
 }
+
+// delete education
+export const deleteEducation = id => async dispatch => {
+	try {
+		const res = await axios.delete(`/api/profile/education/${id}`);
+
+		dispatch({
+			type: UPDATE_PROFILE,
+			payload: res.data
+		});
+
+		dispatch(setAlert('Education Removed', 'success'));
+	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		});
+	}
+}
