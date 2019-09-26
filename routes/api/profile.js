7f239be09b14a9f00aac810c71e616655447e99e
seabassgonzalez <seabassgonzalez@gmail.com>
2019-09-26 15:58:07 -0700

    Implement delete Account button

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route 	GET api/profile/me
 // @desc 	Get current users profile
