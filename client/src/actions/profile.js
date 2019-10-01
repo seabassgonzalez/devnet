3c0de362a9869f234aba5ae693b6a9160be501fa
seabassgonzalez <seabassgonzalez@gmail.com>
2019-09-30 18:59:55 -0700

    Implement GET_PROFILES action, handle case in reducer

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,6 +3,7 @@ import { setAlert } from './alert';
 
 import {
 	GET_PROFILE,
+	GET_PROFILES,
 	PROFILE_ERROR,
 	UPDATE_PROFILE,
 	CLEAR_PROFILE,
@@ -16,6 +17,25 @@ export const getCurrentProfile = () => async dispatch => {
 
 		dispatch({
 			type: GET_PROFILE,
+			payload: res.data
+		});
+	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		});
+	}
+};
+
+// get all profiles
+export const getProfiles = () => async dispatch => {
+	dispatch({ type: CLEAR_PROFILE });
+
+	try {
+		const res = await axios.get('/api/profile');
+
+		dispatch({
+			type: GET_PROFILES,
 			payload: res.data
 		});
 	} catch (err) {
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -138,7 +138,7 @@ router.get('/user/:user_id', async (req, res) => {
 router.delete('/', auth, async (req, res) => {
 	try {
 		// Remove user posts
-		await Post.deleteMany({ user: req.user.id });
+		await Post.deleteMany({ user: req.user.id });	
 		// Remove profile
 		await Profile.findOneAndRemove({ user: req.user.id });
 		// Remove user
