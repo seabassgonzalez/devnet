41621b2c10e2bdd676d12855936500466beb4216
seabassgonzalez <seabassgonzalez@gmail.com>
2019-10-13 11:11:14 -0700

    Link to view profile

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -17,6 +17,9 @@ const ProfileItem = ({
 			<h2>{name}</h2>
 			<p>{status} {company && <span> at {company}</span>}</p>
 			<p className='my-1'>{location && <span>{location}</span>}</p>
+			<Link to={`/profile/${_id}`} className='btn btn-primary'>
+				View Profile
+			</Link>
 		</div>
 	</div>;
 };
