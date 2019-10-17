451365928201808a242301bb99dfe3d86d229ccc
seabassgonzalez <seabassgonzalez@gmail.com>
2019-10-16 20:17:47 -0700

    Add Route for profile in app, implement spinner and buttons in Profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
@@ -7,12 +8,21 @@ import { getProfileById } from '../../actions/profile';
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
 	useEffect(() => {
 		getProfileById(match.params.id);
-	});
+	}, [getProfileById]);
 
 	return (
-		<div>
-			profile
-		</div>
+		<Fragment>
+			{profile === null || loading ? <Spinner /> : <Fragment>
+				<Link to='/profiles' className='btn btn-light'>
+					Back To Profiles
+				</Link>
+				{auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (
+					<Link to='/edit-profile' className='btn btn-dark'>
+						Edit Profile
+					</Link>
+				)}
+			</Fragment>}
+		</Fragment>
 	)
 }
 
