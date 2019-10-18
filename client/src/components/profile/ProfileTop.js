2e202f74dbde1545d888257d806e021e5e5e6874
seabassgonzalez <seabassgonzalez@gmail.com>
2019-10-18 11:17:55 -0700

    Initial ProfileTop component

---
+++ client/src/components/profile/ProfileTop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProfileTop = props => {
+	return (
+		<div></div>
+	)
+}
+
+ProfileTop.propTypes = {
+
+}
+
+export default ProfileTop;
