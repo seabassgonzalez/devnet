1b66b4d87b28adcaf101cb2c341b28cd8a124c25
seabassgonzalez <seabassgonzalez@gmail.com>
2019-10-18 15:35:32 -0700

    Import ProfileGithub and make div for it passing in githubusername, initial ProfileGithub

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
@@ -45,6 +46,10 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
 								))}
 							</Fragment>) : (<h4>No education credentials</h4>)}
 						</div>
+
+						{profile.githubusername && (
+							<ProfileGithub username={profile.githubusername} />
+						)}
 					</div>
 				</Fragment>
 			)}
