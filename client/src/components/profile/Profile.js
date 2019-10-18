4216b0ed127a70a5b53746498fe62c1897e9a7bf
seabassgonzalez <seabassgonzalez@gmail.com>
2019-10-18 14:50:11 -0700

    Initial ProfileEducation file, import ProfileEducation component into Profile and create div to hold map of each entry

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,7 @@ import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
@@ -35,6 +36,14 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
 									<ProfileExperience key={experience._id} experience={experience} />	
 								))}
 							</Fragment>) : (<h4>No experience credentials</h4>)}
+						</div>
+						<div className='profile-edu bg-white p-2'>
+							<h2 className='text-primary'>Education</h2>
+							{profile.education.length > 0 ? (<Fragment>
+								{profile.education.map(education => (
+									<ProfileEducation key={education._id} experience={education} />	
+								))}
+							</Fragment>) : (<h4>No education credentials</h4>)}
 						</div>
 					</div>
 				</Fragment>
