8279867233bf7867798f981d79356f4234d7e4de
seabassgonzalez <seabassgonzalez@gmail.com>
2019-10-19 12:18:45 -0700

    Implement Posts component, render link to it in Navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -12,6 +12,11 @@ const Navbar = ({ auth: {isAuthenticated, loading }, logout }) => {
             Developers
           </Link>
       </li>
+       <li>
+          <Link to='/posts'>
+            Posts
+          </Link>
+      </li>
 			<li>
        		<Link to='/dashboard'>
        			<i className='fas fa-user' />{' '}
