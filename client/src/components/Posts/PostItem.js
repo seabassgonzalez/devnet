c0603a9d058993e106f142d3ac3164655e4137bd
seabassgonzalez <seabassgonzalez@gmail.com>
2019-10-20 16:01:04 -0700

    Implement PostItem component

--- client/src/components/Posts/PostItem.js
+++ client/src/components/Posts/PostItem.js
@@ -4,13 +4,48 @@ import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
 
-const PostItem = props =>{
-	return (
-		<div>
-
-		</div>
-	)
-};
+const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date }
+}) => (
+		<div class="post bg-white p-1 my-1">
+          <div>
+            <a href="profile.html">
+              <img
+                class="round-img"
+                src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
+                alt=""
+              />
+              <h4>John Doe</h4>
+            </a>
+          </div>
+          <div>
+            <p class="my-1">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+              possimus corporis sunt necessitatibus! Minus nesciunt soluta
+              suscipit nobis. Amet accusamus distinctio cupiditate blanditiis
+              dolor? Illo perferendis eveniet cum cupiditate aliquam?
+            </p>
+             <p class="post-date">
+                Posted on 04/16/2019
+            </p>
+            <button type="button" class="btn btn-light">
+              <i class="fas fa-thumbs-up"></i>
+              <span>4</span>
+            </button>
+            <button type="button" class="btn btn-light">
+              <i class="fas fa-thumbs-down"></i>
+            </button>
+            <a href="post.html" class="btn btn-primary">
+              Discussion <span class='comment-count'>2</span>
+            </a>
+            <button      
+            type="button"
+            class="btn btn-danger"
+          >
+            <i class="fas fa-times"></i>
+          </button>
+          </div>
+        </div>
+	);
 
 PostItem.propTypes = {
 	post: PropTypes.object.isRequired,
