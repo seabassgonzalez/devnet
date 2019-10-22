8ffca0e99c5e46f77d006632108d7fd5cdc3024d
seabassgonzalez <seabassgonzalez@gmail.com>
2019-10-21 21:27:14 -0700

    Render likes and comment counts depending on whether there are any to render

--- client/src/components/Posts/PostItem.js
+++ client/src/components/Posts/PostItem.js
@@ -6,40 +6,44 @@ import { connect } from 'react-redux';
 
 const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, comments, date }
 }) => (
-		<div class="post bg-white p-1 my-1">
+		<div className='post bg-white p-1 my-1'>
           <div>
-            <a href="profile.html">
+            <a href='profile.html'>
               <img
-                class="round-img"
+                className='round-img'
                 src={avatar}
-                alt=""
+                alt=''
               />
               <h4>{name}</h4>
             </a>
           </div>
           <div>
-            <p class="my-1">
+            <p className='my-1'>
               {text}
             </p>
-             <p class="post-date">
-                Posted on 04/16/2019
+             <p className='post-date'>
+                Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
             </p>
-            <button type="button" class="btn btn-light">
-              <i class="fas fa-thumbs-up"></i>
-              <span>4</span>
+            <button type='button' className='btn btn-light'>
+              <i className='fas fa-thumbs-up'></i>{' '}
+              <span>{likes.length > 0 && (
+              		<span>{likes.length}</span>
+              	)}</span>
             </button>
-            <button type="button" class="btn btn-light">
-              <i class="fas fa-thumbs-down"></i>
+            <button type='button' className='btn btn-light'>
+              <i className='fas fa-thumbs-down'></i>
             </button>
-            <a href="post.html" class="btn btn-primary">
-              Discussion <span class='comment-count'>2</span>
-            </a>
-            <button      
-            type="button"
-            class="btn btn-danger"
-          >
-            <i class="fas fa-times"></i>
-          </button>
+            <Link to={`/post/${_id}`} className='btn btn-primary'>
+              Discussion{' '}
+              	{comments.length > 0 && (
+              		<span className='comment-count'>{comments.length}</span>
+              	)}
+            </Link>
+            {!auth.loading && user === auth.user._id && (
+	            <button type='button' className='btn btn-danger'>
+	            	<i className='fas fa-times'></i>
+	          	</button>
+            )}
           </div>
         </div>
 	);
