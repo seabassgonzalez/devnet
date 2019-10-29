6ee57443a756b97a433813792b5971af805641dd
seabassgonzalez <seabassgonzalez@gmail.com>
2019-10-28 20:14:52 -0700

    Implement getPost action, export get post type, getPost reducer

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST, GET_POST } from './types';
 
 // get posts
 export const getPosts = () => async dispatch => {
@@ -85,6 +85,22 @@ export const addPost = formData => async dispatch => {
 		});
 
 		dispatch(setAlert('Post Created', 'success'));
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		});
+	}
+};
+
+// get post
+export const getPost = id => async dispatch => {
+	try {
+		const res = await axios.get(`/api/posts/${id}`);
+		dispatch({
+			type: GET_POST,
+			payload: res.data
+		});
 	} catch (err) {
 		dispatch({
 			type: POST_ERROR,
