12345a47336f02d592e39e6463ed8de2f9b8029b
seabassgonzalez <seabassgonzalez@gmail.com>
2019-11-29 15:00:01 -0800

    Revert "Button to handle deleteComment"
    This reverts commit e2c10a746ee8dc7f74980db72e0b212a8d012729.

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -3,13 +3,10 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
-import { deleteComment } from '../../actions/post';
 
 const CommentItem = ({
 	postId,
-	comment: { _id, text, name, avatar, user, date },
-	auth,
-	deleteComment
+	comment: { _id, text, name, avatar, user, date }
 }) => (
 		<div class='post bg-white p-1 my-1'>
           <div>
@@ -29,15 +26,6 @@ const CommentItem = ({
              <p class='post-date'>
                 Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
             </p>
-            {!auth.loading && user === auth.user._id && (
-            	<button 
-            		onClick={e => deleteComment(postId, _id)}
-            		type='button'
-            		className='btn btn-danger'
-            	>
-            		<i className='fas fa-times' />
-            	</button>
-            )}
           </div>
         </div>
 	);
@@ -45,12 +33,11 @@ const CommentItem = ({
 CommentItem.propTypes = {
 	postId: PropTypes.number.isRequired,
 	comment: PropTypes.object.isRequired,
-	auth: PropTypes.object.isRequired,
-	deleteComment: PropTypes.func.isRequired
+	auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
 	auth: state.auth
 });
 
-export default connect(mapStateToProps, { deleteComment })(CommentItem);
+export default connect(mapStateToProps, { })(CommentItem);
