d20e8d32a59c07b487faffb1f09663599e6edd04
seabassgonzalez <seabassgonzalez@gmail.com>
2020-01-23 15:14:57 -0800

    Tracing heroku build bug, update initial state in alert reducer to an object

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,8 +1,8 @@
 import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
-const initialState = [
+const initialState = {[
 
-];
+]};
 
 export default function(state = initialState, action){
 	const { type, payload } = action;
